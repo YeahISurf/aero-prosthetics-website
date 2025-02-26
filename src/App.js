015b035454bf36e822556b0c323debd35d9d27a8
YeahISurf <91082396+YeahISurf@users.noreply.github.com>
2025-02-26 04:16:23 -0500

    Update App.js to include Legal page routes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ServiceDetail from './pages/ServiceDetail';
 import Resources from './pages/Resources';
 import Locations from './pages/Locations';
 import Contact from './pages/Contact';
+import Legal from './pages/Legal';
 import NotFound from './pages/NotFound';
 
 function App() {
@@ -26,16 +27,33 @@ function App() {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
+            
+            {/* About Routes */}
             <Route path="/about" element={<About />} />
             <Route path="/about/mission" element={<About />} />
             <Route path="/about/difference" element={<About />} />
             <Route path="/about/team" element={<About />} />
+            
+            {/* Services Routes */}
             <Route path="/services" element={<Services />} />
             <Route path="/services/:serviceId" element={<ServiceDetail />} />
+            
+            {/* Resources Routes */}
             <Route path="/resources" element={<Resources />} />
             <Route path="/resources/:resourceType" element={<Resources />} />
+            
+            {/* Location & Contact Routes */}
             <Route path="/locations" element={<Locations />} />
             <Route path="/contact" element={<Contact />} />
+            
+            {/* Legal Pages */}
+            <Route path="/legal" element={<Legal />} />
+            <Route path="/legal/:pageType" element={<Legal />} />
+            <Route path="/privacy" element={<Legal pageType="privacy" />} />
+            <Route path="/terms" element={<Legal pageType="terms" />} />
+            <Route path="/accessibility" element={<Legal pageType="accessibility" />} />
+            
+            {/* 404 Route */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
